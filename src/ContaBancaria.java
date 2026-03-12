81dc7bdbbc573d842f1b0343cee8c5f3bb721692
Nicolas Almeida <nicolas.alch@outlook.com>
2026-03-11 21:52:48 -0300

    Adicionando as opções: Gerar cartão virtual e Consulta cotações

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,7 +1,13 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class ContaBancaria {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
         String nome = "Peter Parker";
         String conta = "Corrente";
@@ -9,6 +15,16 @@ public class ContaBancaria {
         int opcao = 0;
         double valorAReceber;
         double valorAEnviar ;
+        int digitos = 16;
+        int codigoCvc = 397;
+        String moeda;
+
+        long min = (long) Math.pow(10, digitos - 1);
+        long max = (long) Math.pow(10, digitos) - 1;
+
+        SecureRandom secureRandom = new SecureRandom();
+        long numeroDoCartao = secureRandom.nextLong((max - min) + 1) + min;
+
 
 
         System.out.println("***********************");
@@ -23,12 +39,14 @@ public class ContaBancaria {
                     1- Consultar saldos
                     2- Receber valor
                     3- Transferir valor
-                    4- Sair
+                    4- Gerar cartão virtual
+                    5- Consulta cotações
+                    6- Sair
                     
                     Digite a opção desejada:
                     """;
 
-        while (opcao != 4) {
+        while (opcao != 6) {
             System.out.println(menu);
             opcao = scanner.nextInt();
 
@@ -57,6 +75,33 @@ public class ContaBancaria {
                     break;
 
                 case 4:
+                    System.out.println("Nome do titular: " + nome +
+                            "\nNúmero do cartão: " + numeroDoCartao +
+                            "\nValidade: 05/2036" +
+                            "\nCVC: " + codigoCvc);
+                    break;
+
+                case 5:
+                    scanner.nextLine(); // limpa o ENTER que ficou no buffer
+
+                    System.out.println("Digite a moeda para consulta (Ex: USD-BRL):");
+                    moeda = scanner.nextLine();
+                    if (moeda ==""){
+                        System.out.println("Não encontrei, digite novamente");
+                    }else {
+                        String endereco = "https://economia.awesomeapi.com.br/json/last/" + moeda;
+
+                        HttpClient client = HttpClient.newHttpClient();
+                        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(endereco))
+                                .build();
+                        HttpResponse<String> response = client
+                                .send(request, HttpResponse.BodyHandlers.ofString());
+                        System.out.println(response.body());
+                        break;
+                    }
+
+                case 6:
                     System.out.println("Programa encerrado");
                     break;
 
